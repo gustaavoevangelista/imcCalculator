473fbaa790c2b98788b56a6b3e3d5321cb197327
gustaavoevangelista <gustavo.eng.henrique@gmail.com>
2022-09-14 23:10:21 +0100

    react set and finished

--- imc-calculator/src/App.js
+++ imc-calculator/src/App.js
@@ -1,7 +1,30 @@
-import './app.css' 
 import { useState } from "react"
+import './app.css' 
 
 export default function App(){
+  const [weight, setWeight] = useState('')
+  const [height, setHeight] = useState('')
+  const [message, setMessage] = useState('')
+
+  function calculate(){
+    const hei = height / 100
+    const imc = weight / (hei * hei)
+
+    if(imc< 18.6){
+      setMessage("You are underweight! IMC: " + imc.toFixed(2))
+    }
+    else if (imc >= 18.6 && imc < 24.9){
+      setMessage("Your weight is ideal! IMC: " + imc.toFixed(2))
+    }
+    else if(imc >= 24.9 && imc < 34.9){
+      setMessage("Your weight is slightly high! IMC: " + imc.toFixed(2))
+    }
+    else if(imc > 34.9){
+      setMessage("You are overweight! IMC: " + imc.toFixed(2))
+    }
+  }
+
+
   return(
     <div className='app'>
       <h1>
@@ -10,14 +33,24 @@ export default function App(){
       <span>Let's calculate your IMC</span>
 
       <div className='inputs'>
-        <input type="text" placeholder='weight (kg)' />
+        <input 
+          type="text" 
+          placeholder='weight (kg)' 
+          value={weight}
+          onChange={(e) => setWeight(e.target.value)}
+        />
           
-        <input type="text" placeholder='height (cm)'/>
+        <input 
+          type="text" 
+          placeholder='height (cm)' 
+          value={height} 
+          onChange={(e) => setHeight(e.target.value)}
+        />
 
-        <button >Calculate</button>
+        <button onClick={calculate}>Calculate</button>
       </div>
 
-      <h2>Result</h2>
+      <h2>{message}</h2>
     </div>
   )
 }
